6fce58af58045470f770d98b4041f16c3a9d9880
Syca <3535327017@qq.com>
2022-10-04 12:52:34 +0800

    Let's GO!

---
+++ GrasscutterTools/Utils/ReleaseAPI.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace GrasscutterTools.Utils
+{
+    internal class ReleaseAPI
+    {
+        public static async Task<ReleaseInfo> GetReleasesLastest(string username, string repo)
+        {
+            var headerValue = new ProductInfoHeaderValue("GrasscutterTools", "1");
+            try
+            {
+                HttpHelper.HttpClient.DefaultRequestHeaders.UserAgent.Add(headerValue);
+                return await HttpHelper.GetAsync<ReleaseInfo>($"https://api.github.com/repos/{username}/{repo}/releases/latest");
+            }
+            catch
+            {
+                // 如果Github无法访问，尝试从Gitee获取
+                var release = await HttpHelper.GetAsync<ReleaseInfo>($"https://gitee.com/api/v5/repos/{username}/{repo}/releases/latest");
+                release.Url = $"https://gitee.com/{username}/{repo}/releases";
+                return release;
+            }
+            finally
+            {
+                HttpHelper.HttpClient.DefaultRequestHeaders.UserAgent.Remove(headerValue);
+            }
+        }
+
+        public class ReleaseInfo
+        {
+            [JsonProperty("tag_name")]
+            public string TagName { get; set; }
+
+            [JsonProperty("html_url")]
+            public string Url { get; set; }
+
+            [JsonProperty("created_at")]
+            public DateTimeOffset CraeteTime { get; set; }
+
+            [JsonProperty("name")]
+            public string Name { get; set; }
+
+            [JsonProperty("body")]
+            public string Body { get; set; }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# 如果您不相信我，请您检查代码后自行编译，请勿刻意抹黑本人！
+
+# GrasscutterTool-3.1.5
+> 原神3.1.5GC工具 v2.3.3(based on v1.6.7)
+
+点个免费的star再[拿走](https://github.com/TeyvatL/GrasscutterTool-3.1.5/releases)吧！
+
+Fork from https://github.com/jie65535/GrasscutterCommandGenerator
+
+# 说明
+ - 支持 `简体中文`！
+ - 懒得更新其他语言了
+
+由 [Syca](https://github.com/Sycamore0) 修改，id大部分适配3.1.5X。</br>
+修复1.3.1后Spawn无法正常使用的问题</br>
+欢迎PR
+
+# 内容
+ - 3.2背包物品
+ - 3.2角色
+ - 3.2魔物
+ - 3.2任务
+
+# Credit
+WindSeedClientNotify#7094
+
+庫卡🅺🅾🅺🅰#8595
+
+TQNF#2978
+
+umaru#6658
+
+Sakura616#0807
+
+瑶瑶#1804
